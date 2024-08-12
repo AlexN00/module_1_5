cef3e9727fa8eba8e9a68359008ef81b80dcbd2e
AlexN00 <ya.ratatouille7@gmail.com>
2024-08-12 17:49:40 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 1, 2, True, 'hello'
+print(immutable_var)
+#immutable_var[0][0]=3 #не дает изменить элемент кортеджа так как Элементы кортеджа являются неизменяемыми
+#print(immutable_var)
+mutable_list= [1,2,'hello']
+print(mutable_list)
+mutable_list[0]=4
+print(mutable_list)
